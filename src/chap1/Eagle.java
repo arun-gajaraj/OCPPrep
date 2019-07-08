27a5bc7e02a360ecb9ca70e5174d4b4f1ea6472b
Arun Gajaraj <arun.gajarajan@pearson.com>
2019-07-08 15:23:29 +0530

    Interfaces

--- src/chap1/Eagle.java
+++ src/chap1/Eagle.java
@@ -1,6 +1,6 @@
 package chap1;
 
-public class Eagle implements Fly{
+public class Eagle implements Fly {
 
 	@Override
 	public int getWingSpan() {
@@ -9,14 +9,24 @@ public class Eagle implements Fly{
 
 	@Override
 	public void land() {
-		System.out.println("Animal is landing");
-		
+		System.out.println("Animal is landing in Eagle ");
+
 	}
 
 	public static void main(String[] args) {
-		
+
 		Fly fly = new Eagle();
 		fly.land();
-		
+
+		double speed = Fly.calculateSpeed(100, 2);
+		System.out.println("Speed is " + speed);
+
+		@SuppressWarnings({ "static-access", "unused" })
+		int maxSpeed = fly.MAX_SPEED;
+
+		@SuppressWarnings("unused")
+		int maxSpeed2 = Fly.MAX_SPEED;
+
 	}
+
 }
--- src/chap1/Fly.java
+++ src/chap1/Fly.java
@@ -3,7 +3,15 @@ package chap1;
 public interface Fly {
 
 	public int MAX_SPEED = 100;
-	public int getWingSpan();
-	
-	public void land();
+
+	public int getWingSpan() throws Exception;
+
+	public default void land() {
+
+		System.out.println("Animal is landing in Interface !");
+	}
+
+	public static double calculateSpeed(double distance, double time) {
+		return distance / time;
+	}
 }
